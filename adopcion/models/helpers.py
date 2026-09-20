18b8332204f79115514661eae85e950c19268558
udjosea <56905216+udjosea@users.noreply.github.com>
2026-09-20 02:58:47 -0500

    Refactor buscar_mascota function for clarity

--- adopcion/models/helpers.py
+++ adopcion/models/helpers.py
@@ -1,6 +1,12 @@
 # Función auxiliar para buscar una mascota en una lista
-def buscar_mascota(nombre: str, lista_mascotas: list):
+def buscar_mascota(nombre, lista_mascotas):
+    for mascota in lista_mascotas:
+        if mascota.nombre.lower() == nombre.lower():
+            return mascota
+    return None
+""" def buscar_mascota(nombre: str, lista_mascotas: list):
     for mascota in lista_mascotas:
         if mascota.nombre.lower() == nombre.lower():  # Ignora mayúsculas/minúsculas
             return mascota
     return None  # Si no se encuentra, retorna None
+ """
